c5b4832bc6f72092f70b8009d072de6b75d0fd50
sinetek <pitwuu@gmail.com>
2019-06-07 12:48:46 +0300

    Verium: fix about box.

--- src/init.cpp
+++ src/init.cpp
@@ -547,8 +547,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
-    const std::string URL_WEBSITE = "<https://bitcoincore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/VeriumReserve/verium>";
+    const std::string URL_WEBSITE = "<https://vericoin.info>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
