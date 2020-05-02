00fe5099d5aa271436ee20c3c4fe759427d7549f
Matthieu 'JP' DERASSE <github@derasse.fr>
2020-05-02 22:13:01 +0200

    fix(arm64): Implement scrypt algo for arm64

--- src/crypto/scrypt-armv8.c
+++ src/crypto/scrypt-armv8.c
@@ -27,12 +27,12 @@
  * online backup system.
  */
 
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <inttypes.h>
 
-#include <endian.h>
+#include <compat/endian.h>
 
 extern void sha256_init(uint32_t *state);
 
@@ -45,7 +45,6 @@ static inline uint32_t swab32(uint32_t v)
 {
 	return __builtin_bswap32(v);
 }
-
 
 static const uint32_t keypad[12] = {
 	0x80000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00000280
@@ -131,8 +130,6 @@ static inline void PBKDF2_SHA256_128_32(uint32_t *tstate, uint32_t *ostate,
 }
 
 
-
-
 #if defined(__aarch64__)
 
 #include <arm_neon.h>
@@ -141,6 +138,149 @@ static inline void PBKDF2_SHA256_128_32(uint32_t *tstate, uint32_t *ostate,
 #define SCRYPT_MAX_WAYS 3
 #define HAVE_SCRYPT_3WAY 1
 #define scrypt_best_throughput() 3
+
+
+
+static const uint32_t sha256_k[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+/* Elementary functions used by SHA256 */
+#define Ch(x, y, z)     ((x & (y ^ z)) ^ z)
+#define Maj(x, y, z)    ((x & (y | z)) | (y & z))
+#define ROTR(x, n)      ((x >> n) | (x << (32 - n)))
+#define S0(x)           (ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22))
+#define S1(x)           (ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25))
+#define s0(x)           (ROTR(x, 7) ^ ROTR(x, 18) ^ (x >> 3))
+#define s1(x)           (ROTR(x, 17) ^ ROTR(x, 19) ^ (x >> 10))
+
+/* SHA256 round function */
+#define RND(a, b, c, d, e, f, g, h, k) \
+    do { \
+        t0 = h + S1(e) + Ch(e, f, g) + k; \
+        t1 = S0(a) + Maj(a, b, c); \
+        d += t0; \
+        h  = t0 + t1; \
+    } while (0)
+
+/* Adjusted round function for rotating state */
+#define RNDr(S, W, i) \
+    RND(S[(64 - i) % 8], S[(65 - i) % 8], \
+        S[(66 - i) % 8], S[(67 - i) % 8], \
+        S[(68 - i) % 8], S[(69 - i) % 8], \
+        S[(70 - i) % 8], S[(71 - i) % 8], \
+        W[i] + sha256_k[i])
+
+/*
+ * SHA256 block compression function.  The 256-bit state is transformed via
+ * the 512-bit input block to produce a new state.
+ */
+void sha256_transform(uint32_t *state, const uint32_t *block, int swap)
+{
+    uint32_t W[64];
+    uint32_t S[8];
+    uint32_t t0, t1;
+    int i;
+
+    /* 1. Prepare message schedule W. */
+    if (swap) {
+        for (i = 0; i < 16; i++)
+            W[i] = swab32(block[i]);
+    } else
+        memcpy(W, block, 64);
+    for (i = 16; i < 64; i += 2) {
+        W[i]   = s1(W[i - 2]) + W[i - 7] + s0(W[i - 15]) + W[i - 16];
+        W[i+1] = s1(W[i - 1]) + W[i - 6] + s0(W[i - 14]) + W[i - 15];
+    }
+
+    /* 2. Initialize working variables. */
+    memcpy(S, state, 32);
+
+    /* 3. Mix. */
+    RNDr(S, W,  0);
+    RNDr(S, W,  1);
+    RNDr(S, W,  2);
+    RNDr(S, W,  3);
+    RNDr(S, W,  4);
+    RNDr(S, W,  5);
+    RNDr(S, W,  6);
+    RNDr(S, W,  7);
+    RNDr(S, W,  8);
+    RNDr(S, W,  9);
+    RNDr(S, W, 10);
+    RNDr(S, W, 11);
+    RNDr(S, W, 12);
+    RNDr(S, W, 13);
+    RNDr(S, W, 14);
+    RNDr(S, W, 15);
+    RNDr(S, W, 16);
+    RNDr(S, W, 17);
+    RNDr(S, W, 18);
+    RNDr(S, W, 19);
+    RNDr(S, W, 20);
+    RNDr(S, W, 21);
+    RNDr(S, W, 22);
+    RNDr(S, W, 23);
+    RNDr(S, W, 24);
+    RNDr(S, W, 25);
+    RNDr(S, W, 26);
+    RNDr(S, W, 27);
+    RNDr(S, W, 28);
+    RNDr(S, W, 29);
+    RNDr(S, W, 30);
+    RNDr(S, W, 31);
+    RNDr(S, W, 32);
+    RNDr(S, W, 33);
+    RNDr(S, W, 34);
+    RNDr(S, W, 35);
+    RNDr(S, W, 36);
+    RNDr(S, W, 37);
+    RNDr(S, W, 38);
+    RNDr(S, W, 39);
+    RNDr(S, W, 40);
+    RNDr(S, W, 41);
+    RNDr(S, W, 42);
+    RNDr(S, W, 43);
+    RNDr(S, W, 44);
+    RNDr(S, W, 45);
+    RNDr(S, W, 46);
+    RNDr(S, W, 47);
+    RNDr(S, W, 48);
+    RNDr(S, W, 49);
+    RNDr(S, W, 50);
+    RNDr(S, W, 51);
+    RNDr(S, W, 52);
+    RNDr(S, W, 53);
+    RNDr(S, W, 54);
+    RNDr(S, W, 55);
+    RNDr(S, W, 56);
+    RNDr(S, W, 57);
+    RNDr(S, W, 58);
+    RNDr(S, W, 59);
+    RNDr(S, W, 60);
+    RNDr(S, W, 61);
+    RNDr(S, W, 62);
+    RNDr(S, W, 63);
+
+    /* 4. Mix local working variables into global state */
+    for (i = 0; i < 8; i++)
+        state[i] += S[i];
+}
 
 static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16])
 {
@@ -329,7 +469,7 @@ static inline void scrypt_shuffle(uint32_t B[16])
 	B[12] = x4; B[13] = x9; B[14] = x14; B[15] = x3;
 }
 
-static inline void scrypt_core_3way(uint32_t B[32 * 3], uint32_t *V, uint32_t N)
+void scrypt_core_3way(uint32_t B[32 * 3], uint32_t *V, uint32_t N)
 {
 	uint32_t* W = V;
 
@@ -1317,170 +1457,6 @@ static inline void scrypt_core_3way(uint32_t B[32 * 3], uint32_t *V, uint32_t N)
 	scrypt_shuffle(&B[0 + 64]);
 	scrypt_shuffle(&B[16 + 64]);
 }
-
-#else
-
-static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16])
-{
-	uint32_t x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x10,x11,x12,x13,x14,x15;
-	int i;
-
-	x00 = (B[ 0] ^= Bx[ 0]);
-	x01 = (B[ 1] ^= Bx[ 1]);
-	x02 = (B[ 2] ^= Bx[ 2]);
-	x03 = (B[ 3] ^= Bx[ 3]);
-	x04 = (B[ 4] ^= Bx[ 4]);
-	x05 = (B[ 5] ^= Bx[ 5]);
-	x06 = (B[ 6] ^= Bx[ 6]);
-	x07 = (B[ 7] ^= Bx[ 7]);
-	x08 = (B[ 8] ^= Bx[ 8]);
-	x09 = (B[ 9] ^= Bx[ 9]);
-	x10 = (B[10] ^= Bx[10]);
-	x11 = (B[11] ^= Bx[11]);
-	x12 = (B[12] ^= Bx[12]);
-	x13 = (B[13] ^= Bx[13]);
-	x14 = (B[14] ^= Bx[14]);
-	x15 = (B[15] ^= Bx[15]);
-	for (i = 0; i < 8; i += 2) {
-		#define R(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
-		/* Operate on columns. */
-		x04 ^= R(x00+x12, 7);	x09 ^= R(x05+x01, 7);
-		x14 ^= R(x10+x06, 7);	x03 ^= R(x15+x11, 7);
-		
-		x08 ^= R(x04+x00, 9);	x13 ^= R(x09+x05, 9);
-		x02 ^= R(x14+x10, 9);	x07 ^= R(x03+x15, 9);
-		
-		x12 ^= R(x08+x04,13);	x01 ^= R(x13+x09,13);
-		x06 ^= R(x02+x14,13);	x11 ^= R(x07+x03,13);
-		
-		x00 ^= R(x12+x08,18);	x05 ^= R(x01+x13,18);
-		x10 ^= R(x06+x02,18);	x15 ^= R(x11+x07,18);
-		
-		/* Operate on rows. */
-		x01 ^= R(x00+x03, 7);	x06 ^= R(x05+x04, 7);
-		x11 ^= R(x10+x09, 7);	x12 ^= R(x15+x14, 7);
-		
-		x02 ^= R(x01+x00, 9);	x07 ^= R(x06+x05, 9);
-		x08 ^= R(x11+x10, 9);	x13 ^= R(x12+x15, 9);
-		
-		x03 ^= R(x02+x01,13);	x04 ^= R(x07+x06,13);
-		x09 ^= R(x08+x11,13);	x14 ^= R(x13+x12,13);
-		
-		x00 ^= R(x03+x02,18);	x05 ^= R(x04+x07,18);
-		x10 ^= R(x09+x08,18);	x15 ^= R(x14+x13,18);
-		#undef R
-	}
-	B[ 0] += x00;
-	B[ 1] += x01;
-	B[ 2] += x02;
-	B[ 3] += x03;
-	B[ 4] += x04;
-	B[ 5] += x05;
-	B[ 6] += x06;
-	B[ 7] += x07;
-	B[ 8] += x08;
-	B[ 9] += x09;
-	B[10] += x10;
-	B[11] += x11;
-	B[12] += x12;
-	B[13] += x13;
-	B[14] += x14;
-	B[15] += x15;
-}
-
-
-static inline void xor_salsa8_prefetch(uint32_t B[16], const uint32_t Bx[16], uint32_t* V, uint32_t N)
-{
-	uint32_t x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x10,x11,x12,x13,x14,x15;
-	int i;
-
-	x00 = (B[ 0] ^= Bx[ 0]);
-	x01 = (B[ 1] ^= Bx[ 1]);
-	x02 = (B[ 2] ^= Bx[ 2]);
-	x03 = (B[ 3] ^= Bx[ 3]);
-	x04 = (B[ 4] ^= Bx[ 4]);
-	x05 = (B[ 5] ^= Bx[ 5]);
-	x06 = (B[ 6] ^= Bx[ 6]);
-	x07 = (B[ 7] ^= Bx[ 7]);
-	x08 = (B[ 8] ^= Bx[ 8]);
-	x09 = (B[ 9] ^= Bx[ 9]);
-	x10 = (B[10] ^= Bx[10]);
-	x11 = (B[11] ^= Bx[11]);
-	x12 = (B[12] ^= Bx[12]);
-	x13 = (B[13] ^= Bx[13]);
-	x14 = (B[14] ^= Bx[14]);
-	x15 = (B[15] ^= Bx[15]);
-	for (i = 0; i < 8; i += 2) {
-		#define R(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
-		/* Operate on columns. */
-		x04 ^= R(x00+x12, 7);	x09 ^= R(x05+x01, 7);
-		x14 ^= R(x10+x06, 7);	x03 ^= R(x15+x11, 7);
-		
-		x08 ^= R(x04+x00, 9);	x13 ^= R(x09+x05, 9);
-		x02 ^= R(x14+x10, 9);	x07 ^= R(x03+x15, 9);
-		
-		x12 ^= R(x08+x04,13);	x01 ^= R(x13+x09,13);
-		x06 ^= R(x02+x14,13);	x11 ^= R(x07+x03,13);
-		
-		x00 ^= R(x12+x08,18);	x05 ^= R(x01+x13,18);
-		x10 ^= R(x06+x02,18);	x15 ^= R(x11+x07,18);
-		
-		/* Operate on rows. */
-		x01 ^= R(x00+x03, 7);	x06 ^= R(x05+x04, 7);
-		x11 ^= R(x10+x09, 7);	x12 ^= R(x15+x14, 7);
-		
-		x02 ^= R(x01+x00, 9);	x07 ^= R(x06+x05, 9);
-		x08 ^= R(x11+x10, 9);	x13 ^= R(x12+x15, 9);
-		
-		x03 ^= R(x02+x01,13);	x04 ^= R(x07+x06,13);
-		x09 ^= R(x08+x11,13);	x14 ^= R(x13+x12,13);
-		
-		x00 ^= R(x03+x02,18);	x05 ^= R(x04+x07,18);
-		x10 ^= R(x09+x08,18);	x15 ^= R(x14+x13,18);
-		#undef R
-	}
-	B[ 0] += x00;
-	uint32_t one = 32 * (B[0] & (N - 1));
-	__builtin_prefetch(&V[one + 0]);
-	__builtin_prefetch(&V[one + 8]);
-	__builtin_prefetch(&V[one + 16]);
-	__builtin_prefetch(&V[one + 24]);
-	asm("":::"memory");
-	B[ 1] += x01;
-	B[ 2] += x02;
-	B[ 3] += x03;
-	B[ 4] += x04;
-	B[ 5] += x05;
-	B[ 6] += x06;
-	B[ 7] += x07;
-	B[ 8] += x08;
-	B[ 9] += x09;
-	B[10] += x10;
-	B[11] += x11;
-	B[12] += x12;
-	B[13] += x13;
-	B[14] += x14;
-	B[15] += x15;
-}
-
-static inline void scrypt_core(uint32_t *X, uint32_t *V, int N)
-{
-	int i;
-
-	for (i = 0; i < N; i++) {
-		memcpy(&V[i * 32], X, 128);
-		xor_salsa8(&X[0], &X[16]);
-		xor_salsa8(&X[16], &X[0]);
-	}
-	for (i = 0; i < N; i++) {
-		uint32_t j = 32 * (X[16] & (N - 1));
-		for (uint8_t k = 0; k < 32; k++)
-			X[k] ^= V[j + k];
-		xor_salsa8(&X[0], &X[16]);
-		xor_salsa8_prefetch(&X[16], &X[0], V, N);
-	}
-}
-
 #endif
 
 #ifndef SCRYPT_MAX_WAYS
